a3d17797aa91f91fa8f14734a9f2ea4f750d3785
0wen26 <47756558+0wen26@users.noreply.github.com>
2025-06-05 00:27:21 +0200

    Centralize icon parsing

--- lib/services/nfc_service.dart
+++ lib/services/nfc_service.dart
@@ -1,20 +1,13 @@
 import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 
-class NfcReadException implements Exception {
-  final String message;
-  NfcReadException(this.message);
-  @override
-  String toString() => message;
-}
-
 class NfcService {
-  static Future<String?> readTagId() async {
+  static Future<String?> readTag() async {
     try {
       final tag = await FlutterNfcKit.poll();
       await FlutterNfcKit.finish();
       return tag.id;
     } catch (e) {
-      throw NfcReadException(e.toString());
+      throw 'Error leyendo NFC: $e';
     }
   }
 }
--- lib/utils/icon_utils.dart
+++ lib/utils/icon_utils.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 /// Converts an icon code point string to [IconData].
-IconData iconFromCodePoint(String codePoint) {
-  return IconData(int.parse(codePoint), fontFamily: 'MaterialIcons');
-}
+IconData iconFromCodePoint(String code) =>
+    IconData(int.parse(code), fontFamily: 'MaterialIcons');
 
